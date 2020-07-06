0b97d43a4bb98e03f2061346b2d4f372796c14e7
Arjun Ashok <arjun2000ashok@gmail.com>
2020-07-06 09:12:46 +0000

    displayed image from 50k set

--- model/dataloader.py
+++ model/dataloader.py
@@ -1,7 +1,3 @@
-"""
-The .zip file is extracted online, and the train and test dataframes are split
-"""
-
 import numpy as np
 import os
 import h5py
@@ -9,6 +5,7 @@ import torch
 import torchvision
 import torchvision.transforms as T
 from PIL import Image
+from zipfile import ZipFile
 
 # Raw dataset class to be created
 
@@ -58,3 +55,63 @@ class NYUDepthDatasetLabelled(torch.utils.data.Dataset):
 
   def __len__(self):   
     return self.length
+
+
+class NYUDepthDatasetRaw(torch.utils.data.Dataset):
+  def __init__(self, dataset, transforms):
+    self.dataset = dataset
+    self.transforms = transforms
+
+  def __getitem__(self, idx):
+    img_mat = self.mat['images'][self.start_offset + idx]
+    img = np.empty([480, 640, 3])
+    img[:,:,0] = img_mat[0,:,:].T
+    img[:,:,1] = img_mat[1,:,:].T
+    img[:,:,2] = img_mat[2,:,:].T
+    img = img.astype('uint8')
+    img = Image.fromarray(img, 'RGB')
+
+    depth_mat = self.mat['depths'][self.start_offset + idx]
+    depth = np.empty([480, 640])
+    depth = depth_mat.T
+    depth = Image.fromarray(depth, 'F')
+
+    # to display depth, divide by 4
+    if self.transforms:
+      img, depth = self.transforms(img, depth)
+
+    return img, depth  
+
+  def __len__(self):   
+    return self.length
+
+def get_zip_file(path):
+  input_zip = ZipFile(path)
+  data = {name: input_zip.read(name) for name in input_zip.namelist()}
+  return data
+
+def get_raw_datasets(path, shuffle_train = True):
+  zip_data = get_zip_file(path)
+
+  nyu_train = []
+  for row in data['data/nyu2_train.csv'].decode('UTF-8').split('\n'):
+    if len(row) > 0:
+      nyu_train.append(row.split(',')) # stores the image and its depth
+
+  nyu_test = []
+  for row in data['data/nyu2_test.csv'].decode('UTF-8').split('\n'):
+    if len(row) > 0:
+      nyu_test.append(row.split(',')) # stores the image and its depth    
+
+  if shuffle_train:
+    random.shuffle(nyu_train)
+
+  # pass the dataframes to the datasets, and return dataloaders ---> decide augmentation later  
+
+
+
+# Function for getting the data loaders(train/val/test)
+# Baseline model as in Laina et. al.
+# Complete dataset(the .zip file)
+# Repeat the process
+# Visualize the filters, literature, ...
--- test.py
+++ test.py
@@ -4,18 +4,6 @@ import model.dataloader as dataloader
 from PIL import Image
 import skimage.io as io
 
-import importlib
-importlib.reload(dataloader)
-
-def impute_depth(depth):
-  depth = np.asarray(depth)
-  depth_ = np.empty([480, 640, 3])
-  depth_[:,:,0] = depth[:,:]
-  depth_[:,:,1] = depth[:,:]
-  depth_[:,:,2] = depth[:,:]
-  return depth_
-
-
 def plot_color(ax, color, title="Color"):
     """Displays a color image from the NYU dataset."""
 
@@ -35,14 +23,12 @@ def main():
   path = 'data/labelled/nyu_depth_v2_labeled.mat'
 
   train_dset = dataloader.NYUDepthDatasetLabelled(path, 'train', 0.8, None)
-  img, depth = train_dset[42]
-
-  depth = impute_depth(depth)
+  img, depth = train_dset[0]
 
   fig = plt.figure("Labeled Dataset Sample", figsize=(12, 5))
   ax = fig.add_subplot(1, 2, 1)
   plot_color(ax, img)
   plt.show()
 
-  io.imshow(depth/4.0)
+  io.imshow(np.asarray(depth))
   io.show()
