0234efef3b83a30df482e7cffd7823165a4f341d
Arjun Ashok <arjun2000ashok@gmail.com>
2020-07-06 12:52:16 +0000

    data loader works

--- model/dataloader.py
+++ model/dataloader.py
@@ -6,20 +6,44 @@ import torchvision
 import torchvision.transforms as T
 from PIL import Image
 from zipfile import ZipFile
+from io import BytesIO
 
-# Raw dataset class to be created
+# All transforms have to be re-written to support consistency between the image and depth transform
+# Mainly ToTensor()
 
-def train_transform(image, depth):
-  img_transforms = T.Compose([
-    T.Resize((640, 480)),
-    T.ToTensor()
-  ])
-  depth_transforms = T.Compose([
-    T.Resize((320, 240)),
-    T.ToTensor()
-  ])
-  
-  return img_transforms(image), depth_transforms(depth)
+class ToTensor(object):
+  def __call__(self, sample, maxDepth = 1000.0):
+    img, depth = sample['img'], sample['depth']
+    img = self.to_torch(img)
+
+    depth = depth.resize((320, 240)) # TODO:modularise the shape  
+
+    depth = self.to_torch(depth)
+
+    depth = depth.float() * maxDepth
+
+    # TODO:clamp between 10 and 1000 if required
+
+    return {'img': img, 'depth': depth}
+
+  def to_torch(self, x):
+    """
+    Takes a PIL Image, normalizes it to be between [0,1] and turns it into a Torch tensor C * H * W
+    """  
+    if not isinstance(x, Image.Image):
+      raise TypeError('Not a PIL Image') 
+    x_numpy = np.asarray(x, dtype = 'float32') / 255.0
+
+    x_torch = torch.from_numpy(x_numpy)
+
+    if len(x_torch.shape) < 3:  # depth case
+      x_torch = x_torch.view(x_torch.shape[0], x_torch.shape[1], 1)
+      
+    x_torch = x_torch.transpose(0, 1).transpose(0, 2).contiguous()  
+
+
+    return x_torch
+
 
 class NYUDepthDatasetLabelled(torch.utils.data.Dataset):
   def __init__(self, path, split, split_ratio, transforms):
@@ -58,39 +82,40 @@ class NYUDepthDatasetLabelled(torch.utils.data.Dataset):
 
 
 class NYUDepthDatasetRaw(torch.utils.data.Dataset):
-  def __init__(self, dataset, transforms):
+  def __init__(self, zip_file, dataset, transforms):
+    self.zip_file = zip_file # the file is dynamically opened using BytesIO 
     self.dataset = dataset
     self.transforms = transforms
+    self.maxDepth = 1000.0 
 
   def __getitem__(self, idx):
-    img_mat = self.mat['images'][self.start_offset + idx]
-    img = np.empty([480, 640, 3])
-    img[:,:,0] = img_mat[0,:,:].T
-    img[:,:,1] = img_mat[1,:,:].T
-    img[:,:,2] = img_mat[2,:,:].T
-    img = img.astype('uint8')
-    img = Image.fromarray(img, 'RGB')
+    sample = self.dataset[idx]
+    img = Image.open(BytesIO(self.zip_file[sample[0]]))
 
-    depth_mat = self.mat['depths'][self.start_offset + idx]
-    depth = np.empty([480, 640])
-    depth = depth_mat.T
-    depth = Image.fromarray(depth, 'F')
+    depth = Image.open(BytesIO(self.zip_file[sample[1]]))
 
-    # to display depth, divide by 4
+    #TODO: see if required to normalise the depth as maxDepth/depth
+    datum = {'img': img, 'depth': depth}
     if self.transforms:
-      img, depth = self.transforms(img, depth)
+      datum = self.transforms(datum) # a composed set of transforms should be passed
 
-    return img, depth  
+    return datum  
 
   def __len__(self):   
-    return self.length
+    return len(self.dataset)
+
+
+def get_train_transforms():
+  return T.Compose([ToTensor()])
+
+
 
 def get_zip_file(path):
   input_zip = ZipFile(path)
   data = {name: input_zip.read(name) for name in input_zip.namelist()}
   return data
 
-def get_raw_datasets(path, shuffle_train = True):
+def get_raw_dataloaders(path, train_val_ratio = 0.8, train_batch_size = 10, val_batch_size = 2, shuffle_train = True, tiny_set = False):
   zip_data = get_zip_file(path)
 
   nyu_train = []
@@ -103,15 +128,35 @@ def get_raw_datasets(path, shuffle_train = True):
     if len(row) > 0:
       nyu_test.append(row.split(',')) # stores the image and its depth    
 
-  if shuffle_train:
-    random.shuffle(nyu_train)
+  num_train = int(len(nyu_train) * train_val_ratio) # the number of training examples to use
 
-  # pass the dataframes to the datasets, and return dataloaders ---> decide augmentation later  
+  nyu_train = nyu_train[0: num_train]
+  nyu_val = nyu_train[num_train :]
+
+  if tiny_set:
+    nyu_train = nyu_train[0: int(0.1 * len(nyu_train))]
+    nyu_val = nyu_val[0 : int(0.1 * len(nyu_val))] 
+    nyu_test = nyu_test[0 : int(0.1 * len(nyu_test))]
+
+  train_dataset = NYUDepthDatasetRaw(zip_data, nyu_train, train_transforms)
+  val_dataset = NYUDepthDatasetRaw(zip_data, nyu_val, test_transforms)
+  test_dataset = NYUDepthDatasetRaw(zip_data, nyu_test, test_transforms)
+
+  # batch size should be provided correspondingly if tiny_dataset is required(for checking)
+  train_dataloader = torch.utils.data.DataLoader(train_dataset, 
+                                                batch_size = train_batch_size,
+                                                shuffle = True,
+                                                num_workers = 4) # check error here for collate_fn
+
+  val_dataloader = torch.utils.data.DataLoader(val_dataset, 
+                                                batch_size = val_batch_size,
+                                                shuffle = True,
+                                                num_workers = 4) 
 
 
+  test_dataloader = torch.utils.data.DataLoader(test_dataset, 
+                                                batch_size = 1,
+                                                shuffle = False,
+                                                num_workers = 2)
 
-# Function for getting the data loaders(train/val/test)
-# Baseline model as in Laina et. al.
-# Complete dataset(the .zip file)
-# Repeat the process
-# Visualize the filters, literature, ...
+  return train_dataloader, val_dataloader, test_dataloader
--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import model.dataloader as dataloader
+from model.dataloader import *
 from PIL import Image
 import skimage.io as io
+from zipfile import ZipFile
+import torchvision.transforms as T
+
+def get_zip_file(path):
+  input_zip = ZipFile(path)
+  data = {name: input_zip.read(name) for name in input_zip.namelist()}
+  return data
+
+
 
 def plot_color(ax, color, title="Color"):
     """Displays a color image from the NYU dataset."""
@@ -19,16 +28,73 @@ def plot_depth(ax, depth, title="Depth"):
     ax.set_title(title)
     ax.imshow(depth)
 
-def main():
-  path = 'data/labelled/nyu_depth_v2_labeled.mat'
+def get_train_transforms():
+  return T.Compose([ToTensor()])
 
-  train_dset = dataloader.NYUDepthDatasetLabelled(path, 'train', 0.8, None)
-  img, depth = train_dset[0]
 
-  fig = plt.figure("Labeled Dataset Sample", figsize=(12, 5))
-  ax = fig.add_subplot(1, 2, 1)
-  plot_color(ax, img)
-  plt.show()
+def main(data):
+  # path = 'data/labelled/nyu_depth_v2_labeled.mat'
 
-  io.imshow(np.asarray(depth))
-  io.show()
+  # train_dset = dataloader.NYUDepthDatasetLabelled(path, 'train', 0.8, None)
+  # img, depth = train_dset[0]
+
+  # fig = plt.figure("Labeled Dataset Sample", figsize=(12, 5))
+  # ax = fig.add_subplot(1, 2, 1)
+  # plot_color(ax, img)
+  # plt.show()
+
+  # io.imshow(np.asarray(depth))
+  # io.show()
+
+  train_val_ratio = 0.8
+
+  nyu_train = []
+  for row in data['data/nyu2_train.csv'].decode('UTF-8').split('\n'):
+    if len(row) > 0:
+      nyu_train.append(row.split(',')) # stores the image and its depth
+
+  nyu_test = []
+  for row in data['data/nyu2_test.csv'].decode('UTF-8').split('\n'):
+    if len(row) > 0:
+      nyu_test.append(row.split(',')) # stores the image and its depth    
+
+  num_train = int(len(nyu_train) * train_val_ratio) # the number of training examples to use
+
+  nyu_val = nyu_train[num_train :]
+  nyu_train = nyu_train[0: num_train]
+
+  dset = NYUDepthDatasetRaw(data, nyu_train, get_train_transforms())
+  sample = dset[0]
+  img, depth = sample['img'], sample['depth']
+
+
+  # Below applied for PIL Image
+  
+  # img = np.asarray(img, dtype = 'float32') / 255.0
+  # depth = np.asarray(depth, dtype = 'float32') / 255.0
+
+  # img = img * 255
+  # depth = depth * 255
+
+
+  # Below for tensor
+
+  # img = img.numpy().transpose(1,2,0) * 255
+  # depth = depth.numpy().transpose(1,2,0) * 255 / 1000
+
+  # img = Image.fromarray(img.astype(np.uint8), mode = 'RGB')
+  # depth = Image.fromarray(depth.astype(np.uint8)[:,:,0], mode='L')
+
+
+
+  # Plot
+
+  # fig = plt.figure("Raw Dataset Sample", figsize=(12, 5))
+  # ax = fig.add_subplot(1, 2, 1)
+  # plot_color(ax, img)
+  # plt.show()
+
+  # io.imshow(np.asarray(depth))
+  # io.show()
+
+
